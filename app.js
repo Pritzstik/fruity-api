07788d6aacf991785ea8cec08e07ca0c71b691e3
pritzstik <pritam.vekaria@gmail.com>
2025-04-08 09:11:15 +0100

    add API project

---
+++ app.js
@@ -0,0 +1,13 @@
+const express = require('express')
+const app = express ()
+const fruits = require('./routes/fruits')
+
+
+app.get('/', (request, response) => {
+    response.status(200).send('Root functioning')
+})
+
+app.use(express.json())
+app.use('/fruits', fruits)
+
+module.exports = app
